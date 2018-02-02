b1445e2d456d66158559be80e07b30e01307b206
Danielalab <olabolacrayola@hotmail.com>
2018-02-01 21:33:32 -0500

    modifica codigo redireccionar

--- public/js/app.js
+++ public/js/app.js
@@ -1,44 +1,4 @@
-// función global de window
-let begin = () => {
-  window.location.href = 'https://danielalab.github.io/card-validator/public/home.html';
-  // selecciona elementos del DOM
-  let cardNumber = document.getElementById('card-number');
-  let name = $('#name');
-  let cvv = document.getElementById('cvv');
-  let month = document.getElementById('month');
-  let year = document.getElementById('year');
-  let btnSubmit = $('#btn-submit');
-  let messageTesting = $('#message');
-  let returnCode = $('#return');
-  console.log(name);
 
-  // declarar funciones 
-  let addElementsHtml = (valueReturnAnielCard) => {
-    returnCode.text(valueReturnAnielCard);      
-    if (valueReturnAnielCard === true) {
-      messageTesting.text(' The card is valid');
-    } else {
-      messageTesting.text(' The card is invalid'); 
-    }
-  };
-
-  let resetExample = () => {
-    cardNumber.value = '';
-    name.value = '';
-    cvv.value = ''; 
-    month.value = '';
-    year.value = '';
-  };
-
-  let testingLibrary = (event) => {
-    event.preventDefault(); 
-    anielCard(name, cardNumber, cvv, month, year)
-    addElementsHtml(anielCard(name, cardNumber, cvv, month, year));
-    resetExample();
-  };
-
-  // asocia eventos a elementos del DOM
-  btnSubmit.on('click', testingLibrary);
+window.onload = function () {
+  window.location.href = 'https://danielalab.github.io/card-validator/public/home.html';  
 };
-
-window.onload = (begin);
